c962c86e2171d84bbea56b907f80d0dbc3cdae67
ruphil <rukmangadanies@gmail.com>
2021-04-20 13:56:55 +0530

    Update index.ts

--- wsTest/index.ts
+++ wsTest/index.ts
@@ -2,6 +2,15 @@ import { Server } from 'ws';
 import { URL } from 'url';
 
 const wss = new Server({ port: 8080 });
+const gamerooms = [
+  {
+    gameroomid: 'sdf-asfsd-34jf',
+    game: 'angryballs',
+    players: ['ws1', 'ws2', 'ws34', 'ws156']
+  }, {
+
+  }
+];
 
 wss.on('connection', function connection(ws, req) {
   let urlObj = new URL(req.url!, 'ws://localhost:8080');
