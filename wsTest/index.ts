d17b242922a1c449d932491f940677815dfc5706
ruphil <rukmangadanies@gmail.com>
2021-04-20 10:09:17 +0530

    p

--- wsTest/index.ts
+++ wsTest/index.ts
@@ -2,9 +2,10 @@ import WebSocket from 'ws';
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    console.log(req.url);
   });
 
   ws.send('something');
