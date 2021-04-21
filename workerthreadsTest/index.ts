cafffab62bdf3383b12f74d6e7a32874cef5a3cf
ruphil <rukmangadanies@gmail.com>
2021-04-21 18:42:07 +0530

    p

--- workerthreadsTest/index.ts
+++ workerthreadsTest/index.ts
@@ -2,18 +2,13 @@ import { Worker } from 'worker_threads';
 
 for(let i = 0; i < 10; i++){
     let dataObj = { 
-        company: "IOC",
-        startDate: 20,
-        endDate:20
+        year: 2001,
+        noofdays: 10
     };
 
-    const worker = new Worker('./emitter.js', { workerData: dataObj});
+    const worker = new Worker('./izipdownloader.js', { workerData: dataObj});
     
     worker.on('message', (msg) => {
         console.log(msg);
     });
-
-    // worker.on('exit', (exitCode) => {
-    //     console.log('One Worker Exited');
-    // });
 }
--- workerthreadsTest/izipdownloader.js
+++ workerthreadsTest/izipdownloader.js
@@ -1,4 +1,5 @@
 const { parentPort, workerData } = require('worker_threads');
 const moment = require('moment');
 
+
 parentPort.postMessage('Finished Processing: ' + workerData.company + moment().toLocaleString());
